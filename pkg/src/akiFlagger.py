4cb9bae0105a987556192d9dddc27df944131aea
Ishan Saran <ishansaran65@gmail.com>
2020-08-09 18:39:33 -0700

    Added generate_toy_data() function to flagger

--- pkg/src/akiFlagger.py
+++ pkg/src/akiFlagger.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 
-__version__ = '0.0.3'
-
 def returnAKIpatients(df, 
                       aki_calc_type = 'both', keep_extra_cols = True, 
                       cond1time = '48hours', cond2time = '168hours', eGFR_impute = False, add_stages=True):
@@ -39,10 +37,6 @@ def returnAKIpatients(df,
 
     Raises:
         AssertionError: If the dataframe is missing an expected column; e.g. if there is no age/sex/race and eGFR_impute is True.
-
-    .. _PEP 484:
-        https://www.python.org/dev/peps/pep-0484/
-
 
     '''
     
@@ -128,8 +122,6 @@ def addBackCalcAKI(df,
     Returns: 
         df (pd.DataFrame): dataframe with back-calculated aki values added in
     '''
-    #backcalc_aki = np.empty(df.shape[0])
-    #backcalc_aki[:] = np.nan
     
     df = df.reset_index(drop=True).set_index('time')
     df = df[~df.index.duplicated()]
@@ -159,7 +151,6 @@ def addRollingWindowAKI(df, add_stages = True,
     Returns: 
         df (pd.DataFrame): dataframe with rolling-window aki values added in
     '''
-    #df = df[~df.duplicated()]
     df = df.set_index('time').sort_index()
     #df = df[~df.duplicated()]
     df_rw = df.loc[df.admission[0] - pd.Timedelta(hours=172):]
@@ -178,33 +169,63 @@ def addRollingWindowAKI(df, add_stages = True,
     
     if add_stages:
         df['stage1'] = (df.deltacreat_48hr >= 0.3) | (df.deltacreat_7day >= 0.5*df.mincreat_7day)
-        df['stage2'] = df.deltacreat_48hr >= 2*df.deltacreat_48hr
-        df['stage3'] = df.deltacreat_48hr >= 3*df.deltacreat_48hr
+        df['stage2'] = df.deltacreat_48hr >= 2*df.mincreat_48hr
+        df['stage3'] = df.deltacreat_48hr >= 3*df.mincreat_48hr
     return df
 
 def eGFRbasedCreatImputation(age, sex, race):
+    '''
+    Imputes the baseline creatinine values for those patients missing outpatient creatinine measurements from 365 to 7 days prior to admisison.
+    The imputation is based on the `CKD-EPI equation <https://www.niddk.nih.gov/health-information/professionals/clinical-tools-patient-management/kidney-disease/laboratory-evaluation/glomerular-filtration-rate/estimating>`_ 
+    from the paper *A New Equation to Estimate Glomerular Filtration Rate (Levey et. Al, 2009)* linked below. 
+
+    We assume a GFR of 75 (mL/min/1.73 m^2) and then estimate a creatinine value based on the patient demographics.
+
+    Equation: https://www.niddk.nih.gov/health-information/professionals/clinical-tools-patient-management/kidney-disease/laboratory-evaluation/glomerular-filtration-rate/estimating
+    
+    Full paper: https://pubmed.ncbi.nlm.nih.gov/19414839/
+
+    Args:
+        age (float or array of floats): the age(s) of the patient(s).
+        sex (bool or array of bools): whether or not the patient is female (female is True).
+        race (bool or array of bools): whether or not the patient is black (black is True).
+
+    Returns:
+        creat (float or array of floats): the imputed creatinine value(s) of the patient(s).
+    '''
+    #The constants kappa and alpha correspond to those used by the authors and the CKD-EPI equation, linked in the docstrings
     kappa  = (0.9 - 0.2*sex)
     alpha  = (-0.411+0.082*sex)
     creat_over_kappa = 75/(141*(1 + 0.018*sex)*(1 + 0.159*race)*0.993**age)
     
+    #Note that if creat/kappa is < 1 then the equation simplifies to creat_over_kappa = (creat/kappa)**(-1.209)
+    # and if creat/kappa is > 1 then the equation simplifies to (creat/kappa)**alpha. Thus, we can replace the min(~)max(~)
+    #statements with the following check to define the creatinine.
     if creat_over_kappa < 1:
         creat = kappa*creat_over_kappa**(-1/1.209)
     elif creat_over_kappa >= 1:
         creat = kappa*creat_over_kappa**(1/alpha)
     return creat
 
-def eGFR(creat, age, female, black):
+def eGFR(creat, age, female, black): 
     '''
-    Calculates the estimated glomerular filtration rate based on the serum creatinine levels, age, sex, and race (black or not black);
-    Based on the formula in the paper A New Equation to Estimate Glomerular Filtration Rate (Levey et. Al, 2009) linked below
+    Calculates the estimated glomerular filtration rate based on the serum creatinine levels, age, sex, and race.
+    Based on the formula in the paper A New Equation to Estimate Glomerular Filtration Rate (Levey et. Al, 2009) linked below.
     
     Equation: https://www.niddk.nih.gov/health-information/professionals/clinical-tools-patient-management/kidney-disease/laboratory-evaluation/glomerular-filtration-rate/estimating
     
     Full paper: https://pubmed.ncbi.nlm.nih.gov/19414839/
     
     Args:
-        creat
+        creat (float or array of floats): the creatinine value(s) of the patient(s).
+        age (float or array of floats): the age(s) of the patient(s).
+        female (bool or array of bools): whether or not the patient is female (female is True).
+        black (bool or array of bools): whether or not the patient is black (black is True).
+
+    Returns:
+        egfr (float or array of floats):
     '''
+    
     min_ck = np.clip(creat/(0.9-0.2*female), a_min=None, a_max=1) #Equivalent to min(cr/k, 1)
     max_ck = np.clip(creat/(0.9-0.2*female), a_min=1, a_max=None) #Equivalent to max(cr/k, 1)
     
@@ -212,4 +233,53 @@ def eGFR(creat, age, female, black):
     
     egfr = 141*(0.993**age)*(min_ck**alpha)*(max_ck**-1.209)*(1+female*0.018)*(1+black*0.159)
     
-    return np.round(egfr, decimals=5) #Helper function to ensure eGFRBasedImputation() works as expected
+    return np.round(egfr, decimals=5) 
+
+def generate_toy_data(num_patients = 100, num_encounters_range = (3, 10), include_demos = False):
+    '''
+    Generates toy data for demonstrating how the flagger works.
+
+    Args:
+        num_patients (int): integer, default 100.
+            Number of patients to generate
+        num_encounters_range (tuple): tuple, default (3, 10).
+            Number of encounters per patient will be randomly selected from a range between this tuple.
+        include_demos (bool): boolean, default False. 
+            Whether or not to include the demographic information in the generated dataset
+
+
+    Returns:
+        df (pd.DataFrame): dataframe with toy numbers to work with.
+
+    '''
+    np.random.seed(0) #seed for reproducibility
+
+    #pick admission dates from Jan 1, 2020 to July 1, 2020 (6 month period) and generate time deltas from +- 5 days
+    date_range = (pd.to_datetime('2020-01-01').value // 10**9, pd.to_datetime('2020-07-01').value // 10**9) 
+    time_deltas = pd.timedelta_range(start='-5 days', end='5 days', freq='6H')
+
+    #Generate random MRN #s, admission dates, and encounters
+    #Generate between 3 and 10 encounters for each patient
+    mrns = np.random.randint(10000, 20000, num_patients)
+    admns = pd.to_datetime(np.random.randint(date_range[0], date_range[1], 100), unit = 's')
+    encs = [np.random.randint(10000, 99999, np.random.randint(num_encounters_range[0],num_encounters_range[1])) for mrn, admn in zip(mrns, admns)]
+    creats = np.clip(np.random.normal(loc = 1, scale = 0.3, size=100), a_min = 0, a_max = None)
+
+    #Combine the two dataframes
+    d1 = pd.DataFrame([mrns, admns, creats]).T
+    d2 = pd.DataFrame(encs)
+
+    d1.columns = ['mrn', 'admission', 'base_creat']
+    d2 = d2.add_prefix('enc_')
+
+    df = pd.concat([d1, d2], axis=1)
+    df = pd.melt(df, id_vars = d1.columns, value_name = 'enc').drop('variable', axis=1)
+
+    #Remove the duplicated & null values and reset the index
+    df = df[np.logical_and(~df.enc.isnull(), ~df.enc.duplicated())].reset_index(drop=True) 
+
+    df['creat'] = np.clip(df.base_creat + np.random.normal(loc = 0, scale = 0.25, size = df.shape[0]), a_min = 0.1, a_max = None).astype('float')
+    df['time'] = df.admission + np.array([random.choice(time_deltas) for i in range(df.shape[0])])
+    df = df[['mrn', 'enc', 'admission', 'time', 'creat']]
+    print('Toy data shape: {}'.format(df.shape))
+    return df
