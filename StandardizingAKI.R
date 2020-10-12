2dd16db983938126bc9f4c2ac3a8bce25317523a
Ishan Saran <ishansaran65@gmail.com>
2020-10-12 13:55:41 -0700

    Working on converting flagger to R; finished bulk of the flagger calculations; needs further testing & variable definitions

--- StandardizingAKI.R
+++ StandardizingAKI.R
@@ -1,11 +1,11 @@
+# Import libraries
 library(data.table)
 library(tidyverse)
 library(zoo)
 
 # User-defined functions
 addRollingWindowAKI <- function(dataframe, window1=as.difftime(2, units='days'), window2=as.difftime(7, units='days')) {
-  #if (missing(window1)) window1 <- as.difftime(2, units='days')
-  #if (missing(window2)) window2 <- as.difftime(7, units='days')
+
   df <- dataframe %>% group_by(mrn) %>%
     mutate(
       # Find the rolling minimum creats for both rolling windows
@@ -13,7 +13,7 @@ addRollingWindowAKI <- function(dataframe, window1=as.difftime(2, units='days'),
       min_creat7d = sapply(time, function(x) min(creat[between(time, x - window2, x)])), # Find minimum creatinine in the past 7 days
       
       condition1 = creat >= min_creat48 + 0.3, # Check if the creat jumps by 0.3; aka KDIGO criterion 1 
-      condition2 = creat >= 1.5*min_creat7d, # Check if the creat jumps by 50%; aka KDIGO criterion 2
+      condition2 = round(creat, digits=4) >= round(1.5*min_creat7d, digits=4), # Check if the creat jumps by 50%; aka KDIGO criterion 2
       
       stage1 = as.integer(condition1 | condition2), # Stage 1 AKI is either 0.3 OR 50% jump
       stage2 = as.integer(creat >= 2*min_creat7d), # Check if creat doubles; aka KDIGO Stage 2 AKI 
@@ -24,23 +24,16 @@ addRollingWindowAKI <- function(dataframe, window1=as.difftime(2, units='days'),
 }
 
 addBackCalcAKI <- function(dataframe) {
-  df <- dataframe %>% rowwise() %>%
-    mutate(
-      t365_7subset = between(time, admission - as.difftime(365, units = 'days'), admission - as.difftime(7, units = 'days')),
-      ) %>% group_by(enc) %>%
-    mutate(
-      baseline_creatinine = round(median(creat[(!inpatient) & (t365_7subset)]), digits=2), 
-      baseline_creat = na.locf(baseline_creatinine),
-      stage1 = as.integer(creat >= 1.5*baseline_creat),
-      stage2 = as.integer(creat >= 2*baseline_creat),
-      stage3 = as.integer(creat >= 3*baseline_creat),
-      bc = stage1 + stage2 + stage3,
-      
-      bc_na = is.na(baseline_creat)
-      ) 
-  df[df$bc_na, 'bc'] = 0
-  df
-  #df %>% select(-t365_7subset, -baseline_creat, -stage1, -stage2, -stage3, -bc_na)
+  
+  dataframe[, baseline_creat := .SD[time >= admission - as.difftime(365, units='days') & time <= admission - as.difftime(7, units='days') & inpatient == F, round(median(creat), 6)], by = mrn]
+  dataframe[time < admission - as.difftime(6, units='hours'), baseline_creat := NA]
+  
+  #Add AKI stages
+  dataframe[, stage1 := (creat >= round(1.5 * baseline_creat, digits=2))]
+  dataframe[, stage2 := (creat >= 2 * baseline_creat)]
+  dataframe[, stage3 := (creat >= 3 * baseline_creat)]
+  dataframe[, bc := stage1 + stage2 + stage3]
+  dataframe[is.na(bc), bc := 0]
 }
 
 # Read in data frame
@@ -53,8 +46,10 @@ dt <- transform(dt, time = as.POSIXct(time, format='%Y-%m-%d %H:%M:%S'),
 sapply(dt, class) # mrn, enc -> int; inpatient -> bool; admission, time -> POSIXct; creat -> numeric
 
 
-# Add in AKI
-aki <- addBackCalcAKI(addRollingWindowAKI(dt))
+# Add AKI
+aki <- addRollingWindowAKI(addBackCalcAKI(dt))
+
+# Test flagger against Python version
 py <- fread('~/Desktop/out.csv')
 # Convert time & admission columns to POSIXct format
 py <- transform(py, time = as.POSIXct(time, format='%Y-%m-%d %H:%M:%S'), 
@@ -65,12 +60,16 @@ sapply(py, class) # mrn, enc -> int; inpatient -> bool; admission, time -> POSIX
 table(aki$rw)
 table(py$rw)
 
-# Compare back-calc vals
+# Compare back-calc vals - 100% match (Oct. 12, 2020)
 table(aki$bc)
 table(py$bc)
 
-# Check the mismatch 
+# Check the mismatch  (if any, None as of now)
 comb <- merge(py, aki, by=c('mrn','time'))
 mismatch <- which(comb$bc.x != comb$bc.y)
 View(comb[mismatch])
-View(aki)
+View(comb)
+
+
+
+
