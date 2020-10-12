f13ab06c010c83a92c4bb10d56a5cb4fe6e25f83
Ishan Saran <ishansaran65@gmail.com>
2020-10-12 06:51:21 -0700

    Working on converting flagger to R

--- StandardizingAKI.R
+++ StandardizingAKI.R
@@ -20,7 +20,7 @@ addRollingWindowAKI <- function(dataframe, window1=as.difftime(2, units='days'),
       stage3 = as.integer(creat >= 3*min_creat7d), # Check if creat triples; aka KDIGO Stage 3 AKI
       rw = stage1 + stage2 + stage3, # The resulting rw column; ultimate output we want added in
     )
-  #df %>% select(-min_creat7d, -min_creat48, -condition1, -condition2, -stage1, -stage2, -stage3)
+  df %>% select(-min_creat7d, -min_creat48, -condition1, -condition2, -stage1, -stage2, -stage3)
 }
 
 addBackCalcAKI <- function(dataframe) {
@@ -29,7 +29,7 @@ addBackCalcAKI <- function(dataframe) {
       t365_7subset = between(time, admission - as.difftime(365, units = 'days'), admission - as.difftime(7, units = 'days')),
       ) %>% group_by(enc) %>%
     mutate(
-      baseline_creatinine = round(median(creat[t365_7subset]), digits=2), 
+      baseline_creatinine = round(median(creat[(!inpatient) & (t365_7subset)]), digits=2), 
       baseline_creat = na.locf(baseline_creatinine),
       stage1 = as.integer(creat >= 1.5*baseline_creat),
       stage2 = as.integer(creat >= 2*baseline_creat),
@@ -44,8 +44,8 @@ addBackCalcAKI <- function(dataframe) {
 }
 
 # Read in data frame
-dt <- fread('~/Desktop/toy.csv')#[1:1000] #Grab the first thousand rows
-#dt <- subset(dt, select=-V1) # Drop the redundant index 
+dt <- fread('~/Desktop/toy.csv')#[1:1000] #Read in dataframe, grab first thousand rows
+dt <- subset(dt, select=-V1) # Drop the redundant index 
 
 # Convert time & admission columns to POSIXct format
 dt <- transform(dt, time = as.POSIXct(time, format='%Y-%m-%d %H:%M:%S'), 
@@ -54,27 +54,23 @@ sapply(dt, class) # mrn, enc -> int; inpatient -> bool; admission, time -> POSIX
 
 
 # Add in AKI
-#aki <- addBackCalcAKI(addRollingWindowAKI(dt))
-ish <- fread('~/Desktop/out.csv')
+aki <- addBackCalcAKI(addRollingWindowAKI(dt))
+py <- fread('~/Desktop/out.csv')
+# Convert time & admission columns to POSIXct format
+py <- transform(py, time = as.POSIXct(time, format='%Y-%m-%d %H:%M:%S'), 
+                admission = as.POSIXct(admission, format='%Y-%m-%d %H:%M:%S'))
+sapply(py, class) # mrn, enc -> int; inpatient -> bool; admission, time -> POSIXct; creat -> numeric
 
-tmp <- dt %>% rowwise() %>%
-  mutate(
-    t365_7subset = between(time, admission - as.difftime(365, units = 'days'), admission - as.difftime(7, units = 'days')),
-    #baseline_creatinine = round(median(creat[t365_7subset]), digits=2),
-  ) %>% group_by(enc) %>%
-  mutate(
-    baseline_creatinine = round(median(creat[t365_7subset]), digits=2),
-  ) %>% ungroup(enc) %>% group_by(mrn) %>%
-  mutate(
-    baseline_creat = na.locf(baseline_creatinine),
-    was_na = baseline_creat != baseline_creatinine
-  )
+# Compare rw vals - 100% match (Oct. 11, 2020)
+table(aki$rw)
+table(py$rw)
 
-(tmp$baseline_creat == tmp$baseline_creatinine)
+# Compare back-calc vals
+table(aki$bc)
+table(py$bc)
 
-View(AP)
-str(AP)
-AP[1:5]
-head(AP)
-ts(AP, frequency=12, start=c(1946,1))
-plot(speech)
+# Check the mismatch 
+comb <- merge(py, aki, by=c('mrn','time'))
+mismatch <- which(comb$bc.x != comb$bc.y)
+View(comb[mismatch])
+View(aki)
