cfa3674e4b6d7cc8b6526c2859a0a174d5bd8288
Ishan Saran <ishansaran65@gmail.com>
2021-03-10 15:23:15 -0500

    Working on validating R flagger with 4-column input

--- Rpkg/akiFlagger/R/returnAKIpatients.R
+++ Rpkg/akiFlagger/R/returnAKIpatients.R
@@ -35,10 +35,16 @@
 
 returnAKIpatients <- function(dataframe, HB_trumping = FALSE, eGFR_impute = FALSE,
                               window1 = as.difftime(2, units='days'), window2 = as.difftime(7, units='days'),
-                              add_min_creat = FALSE, add_baseline_creat = FALSE) {
+                              padding = as.difftime(0, units = 'days'),
+                              add_min_creat = FALSE, add_baseline_creat = FALSE,
+                              add_imputed_admission = FALSE, add_imputed_encounter = FALSE) {
+
   age <- sex <- race <- NULL # Add a visible binding (even if it's null) so R CMD Check doesn't complain
   patient_id <- inpatient <- creatinine <- time <- NULL
   min_creat48 <- min_creat7d <- baseline_creat <- aki <- NULL # Also, erase any variables in case duplicate variable names coexist
+
+  window1 <- window1 + padding
+  window2 <- window2 + padding
 
   # Ensure the data frame being fed in contains the patient_id, inpatient, admission, and time columns
   if (!('patient_id' %in% colnames(dataframe))) return("Sorry! Patient ID not found in dataframe.")
@@ -53,7 +59,9 @@ returnAKIpatients <- function(dataframe, HB_trumping = FALSE, eGFR_impute = FALS
 
   }
 
-  df <- copy(dataframe) # Copy the input so it doesn't modify the data frame in place
+  df <- dataframe[, .(patient_id, inpatient, creatinine, time)] # Select the columns of interest
+  df <- df[!duplicated(df)] # Remove duplicated rows
+  df <- df[order(time), .SD ,by = patient_id]
 
   # Rolling minimum creatinine values in the past 48 hours and 7 days, respectively
   df[, min_creat48 := sapply(.SD[, time], function(x) min(creatinine[between(.SD[, time], x - window1, x)])), by=patient_id]
@@ -61,20 +69,27 @@ returnAKIpatients <- function(dataframe, HB_trumping = FALSE, eGFR_impute = FALS
 
   # If HB_trumping is set to True, we want to add condition2, then do RM calcs, then do HB cacls, then add condition1 back in
   if (HB_trumping){
-    df[, delta_t := shift(time, type = 'lead') - time, by = patient_id]
-    df[, inp_lag := shift(inpatient), by = patient_id]
-    df[, inp_lead:= shift(inpatient, type = 'lead'), by = patient_id]
 
-    cond1 = df[, delta_t] >= 72*3600 # 72 hours in units of seconds
-    cond2 = df[, inpatient] & df[, inp_lag]
-    cond3 = df[, inpatient] & !df[, inp_lead]
-    df[, admit_mask := cond1 & cond2 & cond3]
+    # Impute estimated admission and encounter columns
+    df[, delta_t := shift(time, type = 'lead') - time, by = patient_id] # Time difference between current row and next
+    df[, inp_lag := shift(inpatient, fill = F), by = patient_id] # Inpatient column shifted forward
+    df[, inp_lead:= shift(inpatient, type = 'lead'), by = patient_id] # Inpatient column shifted backwards
 
-    return(df)
+    cond1 <- df[, delta_t] <= 72*3600 # Check to make sure times are within 72 hours (in units of seconds) from each other
+    cond2 <- df[, inpatient] & df[, inp_lead] # Ensure that a True is followed by a True (i.e. at least 2 visits)
+    cond3 <- df[, inpatient] & !df[, inp_lag] # Ensure that a True is preceded by a False (i.e. outpatient -> inpatient transition)
+
+    df[cond1 & cond2 & cond3, imputed_admission := time]
+    df[, imputed_admission := na.locf(na.locf(imputed_admission, na.rm = F), fromLast = T), by = patient_id] # Forward-fill then back-fill
+    df[, imputed_encounter_id := .GRP, by = c('imputed_admission', 'patient_id')] # Group by encounter and count each group
+
+    # Remove delta_t, inp_lag, & inp_lead from the dataframe
+    df <- df %>% select(-delta_t, -inp_lag, -inp_lead)
+
     # Baseline creatinine is defined as the median of the outpatient creatinine values from 365 to 7 days prior to admission
-    df[, baseline_creat := .SD[time >= admission - as.difftime(365, units='days') &
-                                 time <= admission - as.difftime(7, units='days') & inpatient == F,
-                               round(median(creatinine), 4)], by = encounter_id]
+    df[, baseline_creat := .SD[time >= imputed_admission - as.difftime(365, units='days') &
+                                 time <= imputed_admission - as.difftime(7, units='days') & inpatient == F,
+                               round(median(creatinine), 4)], by = imputed_encounter_id]
 
     # Imputation of baseline creatinine from CKD-EPI equation; i.e. eGFR-based imputation (assuming an eGFR of 75)
     if (eGFR_impute) {
@@ -98,8 +113,8 @@ returnAKIpatients <- function(dataframe, HB_trumping = FALSE, eGFR_impute = FALS
     condition2 <- round(df[, creatinine], digits=4) >= round(1.5*df[, min_creat7d], digits=4) # Check if the creat increases by 50%; KDIGO criterion 2
 
     # Masks from admission to 2 & 7 days into the future, respectively
-    mask_2d <- (df[, time] >= df[, admission]) & (df[, time] <= df[, admission] + as.difftime(2, units='days'))
-    mask_7d <- (df[, time] >= df[, admission]) & (df[, time] <= df[, admission] + as.difftime(7, units='days'))
+    mask_2d <- (df[, time] >= df[, imputed_admission]) & (df[, time] <= df[, imputed_admission] + window1)
+    mask_7d <- (df[, time] >= df[, imputed_admission]) & (df[, time] <= df[, imputed_admission] + window2)
     bc_mask <- is.na(df[, baseline_creat]) # We don't want to capture null values
     mask <- !bc_mask & mask_7d
 
@@ -123,6 +138,8 @@ returnAKIpatients <- function(dataframe, HB_trumping = FALSE, eGFR_impute = FALS
     mask_rw = (!mask_2d & mask_empty) | bc_mask
     df[mask_rw, aki := condition1[mask_rw]]
 
+    if (!add_imputed_admission) df <- df %>% select(-imputed_admission)
+    if (!add_imputed_encounter) df <- df %>% select(-imputed_encounter_id)
 
   } else {
 
