364f75bace45895ee055b31c95c62b664ef00978
Ishan Saran <ishansaran65@gmail.com>
2022-01-21 21:01:59 -0800

    Added Shiny app scripts; 2022-01-20

--- Rpkg/shinyApp/app.R
+++ Rpkg/shinyApp/app.R
@@ -1,93 +1,135 @@
+## Ishan Saran, Yale School of Medicine
+## Advisor: Dr. Francis Perry Wilson, Professor of Nephrology
+##
+##
+## akiflagger.readthedocs.io
+## pypi.com/project/akiFlagger
+
 library(shiny)
-#library(shinyWidgets)
+library(shinythemes)
+library(VennDiagram)
+library(shinyWidgets)
 library(data.table)
+library(lubridate)
+library(tidyverse)
+library(zoo)
 library(DT)
 
-# Source the main function
-message(list.files("../akiFlagger/R"))
-source("../akiFlagger/R/returnAKIpatients.R")
+source("returnAKIpatients.R")
+baseFolder <- file.path("/Users", "saranmedical-smile", "AKIFlagger")
+dataFolder <- file.path(baseFolder, "data")
+imageFolder <- file.path(baseFolder, "images")
 
-ticker <- 0
-message(ticker)
-# Ideally,
-# toy <- read.csv(text = getURL("htpps://raw.githubusercontent.com/isaranwrap / toy.csv )) # or some central repository
+saveJSON <- function(dataframe, outFP = file.path(dataFolder, "appOUT.json")) {
+  return(write(jsonlite::toJSON(dataframe), file = outFP))
+}
 
-ui <- bootstrapPage(
-  fluidRow(sidebarPanel(
-    fileInput("file1", h4("File input"),
-              accept = c(".csv")), # File input ends here
-
-    checkboxGroupInput("definitions", "Select your definition(s):",
-                       choices = c("RMW", "HBT", "BCI"), inline = TRUE), # Definition selection ends here
-
-    actionButton("calcAKI", "Calculate AKI") # Calculate AKI button here
-
-  ),
+ui <- fluidPage(theme = shinytheme("sandstone"),
+  sidebarPanel(
+    headerPanel(paste("AKI Flagger", today())),
+    fileInput("file", "FILE",
+              accept = "text/csv"), # END FILE INPUT
+    img(src="hex-AKI FlaggeR_github.png", align = "right",
+        height = 64, width = 64), # Image Logo
+    checkboxGroupButtons("definitionSelector",
+                       "DEFINITION:",
+                       c("RMW", "HBT", "BCI"),
+                       selected = c("RMW")),
+                       #inline = TRUE), # END CHECKBOX INPUT
+    sliderInput("padding", "PADDING",
+                value = 4, min = 0, max = 8), # END SLIDER INPUT
+    actionButton("go", "Compute AKI", icon("calculator")), # END ACTION BUTTON
+    downloadButton('download',"Download the data"),
+    uiOutput('downloadData') # END DOWNLOAD BUTTON
+    ),
   mainPanel(
-    verbatimTextOutput("status"),
-    #DT::dataTableOutput("inputTable"),
-    DT::dataTableOutput("outTable")
+    DT::DTOutput("tableOUT")
+  ),
+  fluidRow(
+    wellPanel(
+      plotOutput("venn")
     )
+    # wellPanel(img(src="hex-AKI FlaggeR_github.png",
+    #               height = 46, width = 46),) # Image Logo)
   )
-  #plotOutput()
 )
 
+
+# Define server function
 server <- function(input, output, session) {
 
-  datasetInput <- reactive({
+  tableIN <- reactive({
 
-    req(input$file1)
+    if (!is.null(input$file)) {
+      dataframe <- data.table::fread(input$file$datapath)
 
-    df626 <- data.table::fread(input$file1$datapath)
-
-    # Pre-process
-    df626$time <- as.POSIXct(df626$time)
-    #setDT(df626)
-    df626
-  })
-
-  output$inputTable <- DT::renderDataTable({
-    DT::datatable(datasetInput())
-  })
-
-  output$status <- renderPrint({
-    if (input$calcAKI > 0) {
-      isolate("Calculation complete.")
+      return(dataframe)
     } else {
-      return(paste("akiflagger.readthedocs.io", as.character(typeof(datasetInput()$time))))
+      return()
     }
   })
 
-  output$outTable <- DT::renderDataTable({
-    DT::datatable(
-      datasetInput()
-      )
+  definitionRMW <- reactive(tableIN() %>% returnAKIpatients_RMW(padding = c(input$padding, "hours")))
+  definitionHBT <- reactive(
+    tableIN() %>% returnAKIpatients_HBT(padding = c(input$padding, "hours"))
+  )
+  definitionBCI <- reactive(
+    definitionBCI <- tableIN() %>% returnAKIpatients_BCI(padding = c(input$padding, "hours"))
+  )
+
+  tableREACTIVE <- eventReactive(input$go, {
+    message(length(input$definitionSelector))
+
+    if (length(input$definitionSelector) == 3) {
+      dataframe <- tableIN()
+      dataframe$RMW <- definitionRMW()$aki
+      dataframe$HBT <- definitionHBT()$aki
+      dataframe$BCI <- definitionBCI()$aki
+      return(dataframe)
+
+    } else if (length(input$definitionSelector == 2)) {
+      message("two of the 3 have been selected")
+
+    }
+    if ("RMW" %in% input$definitionSelector) {
+      tableReactive <- tableIN() %>% returnAKIpatients_RMW(padding = c(input$padding, "hours"))
+    } else if ("HBT" %in% input$definitionSelector) {
+      tableReactive <- tableIN() %>% returnAKIpatients_HBT(padding = c(input$padding, "hours"))
+    } else if ("BCI" %in% input$definitionSelector) {
+      tableReactive <- tableIN() %>% returnAKIpatients_BCI(eGFR_impute = T,
+                                        padding = c(input$padding, "hours"))
+    }
+    return(tableReactive)
+  }
+  )
+
+  writeOut <- eventReactive(input$go, {
+    saveJSON(tableREACTIVE())
+    message(list.files(dataFolder))
   })
-    #observeEvent(input$calcAKI, {
-    #})
 
-  output$outTable <- DT::renderDataTable({
-    dInp <- datasetInput()
-
-    dInp$time <- as.POSIXct(dInp$time)
-    dInp <- dInp[order(time), .SD, by = patient_id]
-    dInp[, min_creat48 := sapply(.SD[, time], function(x) min(creatinine[between(.SD[, time], x - as.difftime(2, units = "days"), x)])), by=patient_id]
+  output$venn <- renderPlot({
+    plotAKIoverlap(tableREACTIVE())
   })
 
-  # output$displayTable <- DT::renderDataTable({
-  #   observeEvent(input$calAKI, {
-  #   isolate()
-  #   })
-  # })
-  # Bookmarking - save for now
-  # observe({
-  #   reactiveValuesToList(input)
-  #   session$doBookmark()
-  # })
-  # onBookmarked(updateQueryString)
-  # output$AKI <- eventReactive(input$calcAKI, {
-  #   returnAKIpatients(setDT(isolate(datasetIntermediate())))
-  # })
-}
+  output$download <- downloadHandler(
+    filename = function() {
+      if (input$file$name == "") {
+        paste("Untitled_aki", "csv", sep=".")
+      } else {
+        paste(substr(input$file$name, 1, nchar(input$file$name) - 4), "_aki.csv", sep = "")
+      }
+    },
+    content = function(file) {
+      write.csv(tableREACTIVE(), file, row.names = FALSE)
+    }
+  )
 
-shinyApp(ui, server)
+  output$tableOUT <- DT::renderDT({
+    tableREACTIVE()
+  }, options = list(pageLength = 6))
+} # server
+
+
+# Create Shiny object
+shinyApp(ui = ui, server = server)
