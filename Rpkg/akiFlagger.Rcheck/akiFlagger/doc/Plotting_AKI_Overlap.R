f633b1805a2ac55fff426265bf18fd044b36a368
Ishan Saran <ishansaran65@gmail.com>
2022-01-27 13:52:40 -0800

    Working on pushing new Flagger (including Shiny app, new function(s): returnAKIpatients_* and updated clean(er)) documentation to CRAN

---
+++ Rpkg/akiFlagger.Rcheck/akiFlagger/doc/Plotting_AKI_Overlap.R
@@ -0,0 +1,11 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup, message = FALSE---------------------------------------------------
+library(tidyverse)
+library(akiFlagger)
+library(data.table)
+
