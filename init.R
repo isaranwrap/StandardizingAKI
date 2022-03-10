137c42141fc0e8f8674a9a1a58e25cb7f49441ae
Ishan Saran <ishansaran65@gmail.com>
2022-03-10 11:33:13 -0500

    Adding in init.R and run.R scripts for deploying with Heroku

---
+++ init.R
@@ -0,0 +1,16 @@
+# init.R
+#
+# Example R code to install packages if not already installed
+#
+
+my_packages = c("dplyr", "stringr", "data.table", "shiny",
+                "shinythemes", "VennDiagram", "shinyWidgets",
+                "lubridate", "tidyverse", "zoo", "DT")
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
