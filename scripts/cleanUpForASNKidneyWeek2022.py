9b67f2de17c7a37631bb3f8e37df00c4396e08ae
Ishan Saran <ishansaran65@gmail.com>
2022-08-27 17:38:00 -0500

    Added code for removing race variables from Python and R csv files in documentation

--- scripts/cleanUpForASNKidneyWeek2022.py
+++ scripts/cleanUpForASNKidneyWeek2022.py
@@ -11,18 +11,90 @@ param1 = 'test'
 baseFolder = r'/Users/saranmedical-smile/AKIFlagger/scripts/StandardizingAKI/PyPkg'
 pythonCSVsDirectory = os.path.join(baseFolder, "doc_csvs/python/")
 rCSVsDirectory = os.path.join(baseFolder, "doc_csvs/r/")
-fileNames = [f for f in os.listdir(os.path.join(baseFolder, pythonCSVsDirectory))]
+fileNamesPy = [f for f in os.listdir(os.path.join(baseFolder, pythonCSVsDirectory))]
+filePaths = [os.path.join(baseFolder, pythonCSVsDirectory, f) for f in fileNamesPy]
+
+fileNamesR = [f for f in os.listdir(os.path.join(baseFolder, rCSVsDirectory))]
+filePathsR = [os.path.join(baseFolder, rCSVsDirectory, f) for f in fileNamesR]
+
 
 # General directory structures
-print(fileNames)
+print(fileNamesPy)
 
 
-#file1 = 
+
 
 # Read in data
-egfr_out = pd.read_csv(os.path.join(baseFolder, pythonCSVsDirectory, fileNames[-1])) # ../egfr_out.csv
+example1 = pd.read_csv(filePaths[-4]) # ../example1.csv
+if 'RACE' in example1.columns:
+    example1 = example1.drop('RACE', axis = 1)
+example1.to_csv(filePaths[-4])
+
+# Remove black from dataFRAME and write OUT 
+for file in filePaths:
+    dataFRAME = pd.read_csv(file, index_col = [0])
+    if 'race' in dataFRAME.columns:
+        dataFRAME = dataFRAME.drop('race', axis=1)
+    dataFRAME.to_csv(file)
+# Python done
+
+for file in filePathsR:
+    dataFRAME = pd.read_csv(file, index_col = [0])
+    if 'race' in dataFRAME.columns:
+        dataFRAME = dataFRAME.drop('race', axis=1)
+    dataFRAME.to_csv(file)
+# R done
+
+# 2022-11-02 black / race removed from documentation strings
+
+#3---
+
+# Rename fileNames and filePaths variables
+# Parameters II
 
 
+
+
+# OUTPUT
+#    patient_id                 time  inpatient  creatinine  aki
+# 4       19845  2020-05-08 00:02:54      False        0.76    0
+#                            time  inpatient  creatinine  aki
+# patient_id                                                 
+# 12732       2020-02-23 17:42:42      False        1.42    0
+#    patient_id  inpatient                 time  creatinine
+# 0       12732      False  2020-02-23 23:42:42        1.06
+#    patient_id   age  female  black  inpatient                 time  creatinine
+# 0       12732  64.5    True   True      False  2020-02-23 23:42:42        1.45
+#                            time  inpatient  creatinine  min_creat48  \
+# patient_id                                                            
+# 12732       2020-02-22 17:42:42      False        1.05         1.05   
+
+#             min_creat168  aki  
+# patient_id                     
+# 12732               1.05    0  
+#                            time   age  female  black  inpatient  creatinine  \
+# patient_id                                                                    
+# 12732       2020-02-22 11:42:42  64.5    True   True      False        1.62   
+
+#             baseline_creat  aki  
+# patient_id                       
+# 12732             0.930077    0  
+#                            TIME   AGE   SEX  RACE  INPATIENT  CREATININE  aki
+# PAT_MRN_ID                                                                   
+# 12732       2020-02-22 11:42:42  64.5  True  True      False        1.62    0
+#                            time  inpatient  creatinine  aki
+# patient_id                                                 
+# 12732       2020-02-24 23:42:42      False        1.61    1
+#                            time  inpatient  creatinine  baseline_creat  aki
+# patient_id                                                                 
+# 12732       2020-02-22 23:42:42      False        1.26             NaN    0
+#                            time   age  female  black  inpatient  creatinine  \
+# patient_id                                                                    
+# 12732       2020-02-23 23:42:42  64.5    True   True      False        1.45   
+
+#             baseline_creat  aki  
+# patient_id                       
+# 12732             0.930077    0  
 # Remove black variable
 
 
